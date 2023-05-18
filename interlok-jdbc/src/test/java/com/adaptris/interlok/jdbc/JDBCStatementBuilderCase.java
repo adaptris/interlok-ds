81c946eb4a811a05bc78e8ffc601ee84c5dd1056
Sebastien Belin <sebastien.belin@proagrica.com>
2023-05-18 16:44:50 +0900

    INTERLOK-4088 Uprade to Junit 5
    Upgrade gradle wrapper to 7.6.1

--- interlok-jdbc/src/test/java/com/adaptris/interlok/jdbc/JDBCStatementBuilderCase.java
+++ interlok-jdbc/src/test/java/com/adaptris/interlok/jdbc/JDBCStatementBuilderCase.java
@@ -1,12 +1,5 @@
 package com.adaptris.interlok.jdbc;
 
-import com.adaptris.core.AdaptrisMessage;
-import com.adaptris.core.AdaptrisMessageFactory;
-import com.adaptris.core.jdbc.JdbcConnection;
-import com.adaptris.core.util.JdbcUtil;
-import com.adaptris.interlok.junit.scaffolding.services.JdbcServiceCase;
-import org.junit.Test;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -14,21 +7,29 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 
-public abstract class JDBCStatementBuilderCase extends JdbcServiceCase
-{
+import org.junit.jupiter.api.Test;
+
+import com.adaptris.core.AdaptrisMessage;
+import com.adaptris.core.AdaptrisMessageFactory;
+import com.adaptris.core.jdbc.JdbcConnection;
+import com.adaptris.core.util.JdbcUtil;
+import com.adaptris.interlok.junit.scaffolding.services.JdbcServiceCase;
+
+public abstract class JDBCStatementBuilderCase extends JdbcServiceCase {
   protected static final String JDBC_QUERYSERVICE_DRIVER = "jdbc.queryservice.driver";
   protected static final String JDBC_QUERYSERVICE_URL = "jdbc.queryservice.url";
 
   private static final String NAME = "Rachel";
   private static final String DOB = "1990-03-19";
-  private static final String AGE = Integer.toString(Period.between(LocalDate.parse(DOB, DateTimeFormatter.ISO_LOCAL_DATE), LocalDate.now()).getYears());
+  private static final String AGE = Integer
+      .toString(Period.between(LocalDate.parse(DOB, DateTimeFormatter.ISO_LOCAL_DATE), LocalDate.now()).getYears());
 
   @Test
-  public void testServicesCaptureQuery() throws Exception
-  {
+  public void testServicesCaptureQuery() throws Exception {
     createDatabase();
 
-    JdbcConnection connection = new JdbcConnection(PROPERTIES.getProperty(JDBC_QUERYSERVICE_URL), PROPERTIES.getProperty(JDBC_QUERYSERVICE_DRIVER));
+    JdbcConnection connection = new JdbcConnection(PROPERTIES.getProperty(JDBC_QUERYSERVICE_URL),
+        PROPERTIES.getProperty(JDBC_QUERYSERVICE_DRIVER));
 
     JDBCStatementBuilder service = JDBCCaptureStatementBuilderTest.getService();
     service.setConnection(connection);
@@ -54,26 +55,22 @@ public abstract class JDBCStatementBuilderCase extends JdbcServiceCase
   protected static void createDatabase() throws Exception {
     Connection c = null;
     Statement s = null;
-    try
-    {
+    try {
       Class.forName(PROPERTIES.getProperty(JDBC_QUERYSERVICE_DRIVER));
       c = DriverManager.getConnection(PROPERTIES.getProperty(JDBC_QUERYSERVICE_URL));
       c.setAutoCommit(true);
       s = c.createStatement();
-      try
-      {
+      try {
         s.execute("DROP TABLE person");
-      }
-      catch (Exception e)
-      {
+      } catch (Exception e) {
         // Ignore exceptions from the drop
       }
-      s.execute("CREATE TABLE person " + "(id VARCHAR(128) NOT NULL, name VARCHAR(128) NOT NULL, dob DATE NOT NULL, age INT, inserted_on TIMESTAMP)");
-    }
-    finally
-    {
+      s.execute("CREATE TABLE person "
+          + "(id VARCHAR(128) NOT NULL, name VARCHAR(128) NOT NULL, dob DATE NOT NULL, age INT, inserted_on TIMESTAMP)");
+    } finally {
       JdbcUtil.closeQuietly(s);
       JdbcUtil.closeQuietly(c);
     }
   }
+
 }
